2fb2e6eda26cef975177173d259eeef3c2651c45
OCKAPBIHKA <152022576+OCKAPBIHKA@users.noreply.github.com>
2024-03-31 09:57:09 +0300

    make task -3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,31 @@
 'use strict';
+class StringBuilder {
+  constructor(stringValue) {
+    this.value = stringValue;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  padEnd(str) {
+    this.value += str;
+  }
+
+  padStart(str) {
+    this.value = str + this.value;
+  }
+
+  padBoth(str) {
+    this.value = str + this.value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^. "
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
